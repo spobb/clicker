7e390a8c618782796920bbb924483e7a1cfbc9dc
spobb <gverlaeken@gmail.com>
2024-06-12 15:58:20 +0200

    refactored code

--- building.js
+++ building.js
@@ -1,9 +1,9 @@
-class Building {
+export class Building {
 	constructor(price, production, name) {
 		this._name = name;
 		this._price = price;
 		this._baseCost = price;
-		this._production = production;
+		this._production = production / 20;
 		this._count = 0;
 	}
 	get name() {
@@ -22,61 +22,9 @@ class Building {
 		++this._count;
 	}
 	produce() {
-		return this._production;
+		return this._production * this._count;
 	}
 	updatePrice() {
 		this._price = Math.floor(this._baseCost * 1.15 ** (this._count + 1));
-	}
-}
-
-export class Clicker extends Building {
-	constructor(price, production, name) {
-		super(price, production, name);
-		this._count = 0;
-	}
-	get count() {
-		return this._count;
-	}
-	updateCount() {
-		++this._count;
-	}
-}
-
-export class TurboClicker extends Building {
-	constructor(price, production, name) {
-		super(price, production, name);
-		this._count = 0;
-	}
-	get count() {
-		return this._count;
-	}
-	updateCount() {
-		++this._count;
-	}
-}
-
-export class Balls extends Building {
-	constructor(price, production, name) {
-		super(price, production, name);
-		this._count = 0;
-	}
-	get count() {
-		return this._count;
-	}
-	updateCount() {
-		++this._count;
-	}
-}
-
-export class Portal extends Building {
-	constructor(price, production, name) {
-		super(price, production, name);
-		this._count = 0;
-	}
-	get count() {
-		return this._count;
-	}
-	updateCount() {
-		++this._count;
 	}
 }
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import {Clicker, TurboClicker, Balls, Portal} from './building.js';
+import {Building} from './building.js';
+import {Upgrade} from './upgrade.js';
 import {FloatText} from './floattext.js';
 
 let cookies = 0;
 let clickValue = 1;
 let perTick = 0;
 
-let buildings = [];
 let particles = [];
 
 const button = document.getElementById('button');
@@ -13,11 +13,13 @@ const cookie = document.querySelector('.cookie');
 const cookieDisplay = document.getElementById('cookieDisplay');
 const CPS = document.getElementById('CPS');
 
-const buildingButtons = Array.from(document.querySelectorAll('.building button'));
+const buildingButtons = Array.from(document.querySelectorAll('.building'));
+const buildingsWrapper = document.getElementById('buildings');
 
 button.addEventListener('mousedown', e => {
 	cookies += clickValue;
-	new FloatText(e.clientX, e.clientY, 2000, `+${1}`, particles);
+	cookieDisplay.innerText = `Cookies: ${formatNum(Math.floor((cookies * 1000) / 1000).toFixed(0))}`;
+	new FloatText(e.clientX, e.clientY, 2000, `+${clickValue}`, particles);
 	cookie.classList.add('clicked');
 	addEventListener('mouseup', () => {
 		cookie.classList.remove('clicked');
@@ -29,81 +31,90 @@ document.getElementById('give').addEventListener('click', () => {
 });
 
 // create buildings
-let clicker = new Clicker(10, 0.01, 'Clicker');
-let turbo = new TurboClicker(100, 0.05, 'Turbo Clicker');
-let balls = new Balls(5000, 6, 'Mes couilles');
-let portal = new Portal(10000000, 1000, 'Portal');
+let buildings = {
+	clicker: new Building(15, 0.1, 'Clicker'),
+	turbo: new Building(100, 1, 'Turbo Clicker'),
+	balls: new Building(1100, 8, 'Mes couilles'),
+	portal: new Building(12000, 47, 'Portal'),
+	sweatshop: new Building(130000, 260, 'Chinese Sweatshop'),
+	bank: new Building(1400000, 1400, 'Bank'),
+	temple: new Building(20000000, 7800, 'Temple'),
+};
 
-buildingButtons.forEach(button => {
-	button.addEventListener('click', e => {
-		switch (e.target.id) {
-			case 'clicker':
-				if (cookies >= clicker.price) {
-					// remove cost from total
-					cookies -= clicker.price;
+let clickerup = new Upgrade(5, 1, 'cool upgrade');
 
-					// push to array with all buildings
-					buildings.push(clicker);
+document.querySelector('.upgrade').addEventListener('click', () => {
+	if (cookies >= clickerup.price) {
+		// remove cost from total
+		cookies -= clickerup.price;
 
-					// update info
-					clicker.updatePrice();
-					clicker.updateCount();
+		// update info
+		clickerup.buy();
+		clickValue += Math.round(((perTick * 20) / 100) * clickerup.effect);
+		console.log(Math.round(((perTick * 20) / 100) * clickerup.effect));
+	}
+});
 
-					// update html
-					e.target.parentNode.childNodes[0].innerText = `${formatNum(clicker.price)} cookies `;
-					e.target.parentNode.parentNode.childNodes[3].innerText = `${formatNum(clicker.count)} `;
-				}
-				break;
-		}
-		switch (e.target.id) {
-			case 'turboClicker':
-				if (cookies >= turbo.price) {
-					// remove cost from total
-					cookies -= turbo.price;
+// create each building
 
-					// push to array with all buildings
-					buildings.push(turbo);
+for (const [key, building] of Object.entries(buildings)) {
+	// create html
+	const elemCont = document.createElement('div');
+	elemCont.classList.add('building', 'clickable');
+	elemCont.dataset.is = key;
 
-					// update info
-					turbo.updatePrice();
-					turbo.updateCount();
+	const elemName = document.createElement('span');
+	elemName.innerText = building.name;
 
-					// update html
-					e.target.parentNode.childNodes[0].innerText = `${formatNum(turbo.price)} cookies `;
-					e.target.parentNode.parentNode.childNodes[3].innerText = `${formatNum(turbo.count)} `;
-				}
-				break;
-		}
-		switch (e.target.id) {
-			case 'balls':
-				e.target.parentNode.childNodes[0].innerText = `${formatNum(balls.price)} cookies `;
-				e.target.parentNode.parentNode.childNodes[3].innerText = `${formatNum(balls.count)} `;
+	const elemCounter = document.createElement('span');
+	elemCounter.innerText = `${formatNum(building.count)} `;
 
-				break;
-		}
-		switch (e.target.id) {
-			case 'portal':
-				e.target.parentNode.childNodes[0].innerText = `${formatNum(portal.price)} cookies `;
-				e.target.parentNode.parentNode.childNodes[3].innerText = `${formatNum(portal.count)} `;
-				break;
+	const elemBuyDiv = document.createElement('div');
+	elemBuyDiv.classList.add('buy');
+
+	const elemPrice = document.createElement('span');
+	elemPrice.classList.add('price');
+	elemPrice.innerText = `${formatNum(building.price)} cookies `;
+
+	elemBuyDiv.appendChild(elemPrice);
+	elemCont.append(elemName, elemCounter, elemBuyDiv);
+
+	buildingsWrapper.appendChild(elemCont);
+
+	// event listener on each building
+
+	elemCont.addEventListener('click', () => {
+		// check if player has enough cookies
+		if (cookies >= building.price) {
+			// remove cost from total
+			cookies -= building.price;
+
+			// update info
+			building.updatePrice();
+			building.updateCount();
+
+			// update html
+			elemCounter.innerText = `${formatNum(building.count)} `;
+			elemPrice.innerText = `${formatNum(building.price)} cookies `;
 		}
 	});
-});
+}
 
 function updateCookies() {
 	let produced = 0;
-	buildings.forEach(b => {
-		produced += b.produce();
-	});
-	perTick = produced;
+	for (const [_, building] of Object.entries(buildings)) {
+		produced += building.produce();
+	}
+	perTick = produced * 10;
 	cookies += perTick;
-	cookies = Math.round(cookies * 100) / 100;
-	cookieDisplay.innerText = `Cookies: ${formatNum(Math.floor(cookies))}`;
-	CPS.innerText = `CPS: ${Math.round(perTick * 100) / 5}`;
+	cookies = Math.round(cookies * 1000) / 1000;
+	cookieDisplay.innerText = `Cookies: ${formatNum(Math.floor((cookies * 1000) / 1000).toFixed(0))}`;
+	CPS.innerText = `CPS: ${Math.floor(perTick * 1000) / 50}`;
 }
 
 function formatNum(num) {
-	return Intl.NumberFormat('en', {notation: 'compact'}).format(num);
+	// return Intl.NumberFormat('en', {notation: 'compact'}).format(num);
+	return num;
 }
 
 // game loop
